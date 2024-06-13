e2747b1108466cf8ab655d6bc1a3bec99a8087c5
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-13 17:55:19 +0200

    Initial Commit

---
+++ AppStartup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStartup
+//
+//  Created by Manyuchi, Carrington C on 2024/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
